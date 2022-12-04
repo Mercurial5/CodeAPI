3db7e28b3a0bdb6aaea16cb392f484926c4348fd
Mercurial5 <dias.nespayev@gmail.com>
2022-12-05 00:39:20 +0600

    docs: add description for a class

--- codeapi/languages/Language.py
+++ codeapi/languages/Language.py
@@ -5,4 +5,8 @@ class Language:
 
     All languages have their own separate directory, where their code is
     being executed.
+
+    All languages have property `name` which is super-property. It's used for:
+    > Name of the separate language directory.
+    > Finding class of the language by name.
     """
