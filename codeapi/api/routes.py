e1360a3a8d7315e756e8eea7b0e29b5f2971b282
Nikita Mordvintsev <90397176+Tunaxx-New@users.noreply.github.com>
2022-12-07 00:29:26 +0600

    feat: added case_time as request form

--- codeapi/api/routes.py
+++ codeapi/api/routes.py
@@ -12,9 +12,10 @@ def run():
     weak_outputs = request.form.getlist('weak_outputs')
     strong_inputs = request.form.getlist('strong_inputs')
     strong_outputs = request.form.getlist('strong_outputs')
+    case_time = request.form.get('case_time')
 
     try:
-        result = check(lang, code, weak_inputs, weak_outputs, strong_inputs, strong_outputs)
+        result = check(lang, code, weak_inputs, weak_outputs, strong_inputs, strong_outputs, case_time)
         return result
     except ValueError as e:
         return dict(status=False, reason='error', error=str(e))
