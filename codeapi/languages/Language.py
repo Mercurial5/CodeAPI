7c4f4460b24d3a6cbefe05618aa58e5e7fe92a65
Mercurial5 <dias.nespayev@gmail.com>
2022-12-06 16:11:21 +0600

    feat: get language by name

--- codeapi/languages/Language.py
+++ codeapi/languages/Language.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from abc import ABC, abstractmethod
 import os
 
@@ -14,11 +16,25 @@ class Language(ABC):
     > Name of the separate language directory.
     > Finding class of the language by name.
     """
-    name = None
 
+    # path to the separate directory of language
+    path = None
+    name = None
+    extension = None
 
     def __init__(self):
         self.languages_path = os.getenv('LANGUAGES_PATH')
 
     @abstractmethod
     def run(self, filename: str) -> str: pass
+
+    @staticmethod
+    def get_language_by_name(name: str) -> Language:
+        languages = [language for language_type in Language.__subclasses__() for language in
+                     language_type.__subclasses__()]
+        language = next((language for language in languages if language.name == name), None)
+
+        if language is None:
+            raise ValueError(f'No such language as {name}')
+
+        return language()
--- codeapi/languages/__init__.py
+++ codeapi/languages/__init__.py
@@ -1,3 +1,5 @@
 from codeapi.languages.Language import Language
 from codeapi.languages.Compiled import Compiled
 from codeapi.languages.Interpreted import Interpreted
+from codeapi.languages.interpreted import *
+from codeapi.languages.compiled import *
