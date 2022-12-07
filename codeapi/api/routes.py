66f853fc913214b289629801ef48cbd3ba2f9009
Mercurial5 <dias.nespayev@gmail.com>
2022-12-07 10:39:41 +0600

    feat: check if all data was given correct

--- codeapi/api/routes.py
+++ codeapi/api/routes.py
@@ -14,6 +14,14 @@ def run():
     strong_outputs = request.form.getlist('strong_outputs')
     case_time = request.form.get('case_time')
 
+    if None in (lang, code, weak_inputs, weak_outputs, strong_inputs, strong_outputs, case_time):
+        return dict(status=False, reason='Not all data was given')
+
+    try:
+        case_time = float(case_time)
+    except TypeError:
+        return dict(status=False, reason='case_time should be float')
+
     try:
         result = check(lang, code, weak_inputs, weak_outputs, strong_inputs, strong_outputs, case_time)
         return result
